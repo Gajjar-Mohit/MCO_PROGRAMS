62e4b8a76dfbe1f1ccce6b5de8b0ba740ce79e3d
Gajjar Mohit <114332766+Gajjar-Mohit@users.noreply.github.com>
2022-12-26 12:28:36 +0530

    added the project

---
+++ DecToBin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, counter, i;
+    int binary[32];
+
+    printf("Enter decimal number: ");
+    scanf("%d", &number);
+
+    counter = 0;
+    while (number > 0)
+    {
+        binary[counter] = number % 2;
+        number = number / 2;
+        counter++;
+    }
+    printf("Binary value :");
+    for (i = (counter - 1); i >= 0; i--)
+        printf("%d", binary[i]);
+
+    return 0;
+}
