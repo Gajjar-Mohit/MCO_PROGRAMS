4f09dc224d60af539a707c0cd84b377c29d56cb6
Gajjar Mohit <114332766+Gajjar-Mohit@users.noreply.github.com>
2022-12-26 08:15:03 +0000

    Test commit

--- BintoDec.c
+++ BintoDec.c
@@ -4,21 +4,20 @@
 
 int main()
 {
-    char bin[32] = { 0 };
+    char bin[32] = {0};
     int decimal, i;
-    int counter; 
+    int counter;
 
     printf("Enter the binary value: ");
-    gets(bin);
+    fgets(bin, 10, stdin);
 
     counter = 0;
     decimal = 0;
-    for (i = (strlen(bin) - 1); i >= 0; i--) {
+    for (i = (strlen(bin) - 1); i >= 0; i--)
+    {
         decimal = decimal + (bin[i] - 0x30) * pow((double)2, (double)counter);
         counter++;
     }
-
     printf("Decimal val: %d", decimal);
-
     return 0;
 }
